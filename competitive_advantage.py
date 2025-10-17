fa42f9d371e4691d74c637d6993f42268ae200c7
Gryffin T <Gryffintubon@gmail.com>
2025-10-17 02:32:05 -0500

    Update competitive_advantage.py

--- competitive_advantage.py
+++ competitive_advantage.py
@@ -2,6 +2,7 @@ import streamlit as st
 from finnhub import Client
 from datetime import datetime, timedelta
 import yfinance as yf
+from yfinance import EquityQuery
 
 api_key = st.secrets["FINNHUB_API_KEY"]
 
@@ -18,29 +19,37 @@ def calculate_competitive_advantage(ticker):
 
 
     try:
-        peers = client.company_peers(ticker)
-        average_beta = 0
-        for peer in peers:
-            peer_info = client.company_basic_financials(peer, 'beta')
-            average_beta += peer_info['metric']['beta']
-        average_beta /= len(peers) if peers else st.error("Insufficient peer data to calculate beta index.")
         try:
-            beta_index = ticker_info['metric']['beta']/average_beta
-            if beta_index > 1:
-                if beta_index <= 1.5:
-                    indication = "moderately higher volatility than its peers."
+            peers = client.company_peers(ticker)
+        except:
+            ticker_symbol = ticker_info['info']['symbol']
+            filter = EquityQuery('and', [
+                EquityQuery('is-in', ['sector', ticker_symbol['sector']]),
+                EquityQuery('is-in', ['industry', ticker_symbol['industry']])
+            ])
+            peers = yf.screen[filter]
+            average_beta = 0
+            for peer in peers:
+                peer_info = client.company_basic_financials(peer, 'beta')
+                average_beta += peer_info['metric']['beta']
+            average_beta /= len(peers) if peers else st.error("Insufficient peer data to calculate beta index.")
+            try:
+                beta_index = ticker_info['metric']['beta']/average_beta
+                if beta_index > 1:
+                    if beta_index <= 1.5:
+                        indication = "moderately higher volatility than its peers."
+                    else:
+                        indication = "higher volatility than its peers."
+                elif beta_index < 1:
+                    if beta_index >= -0.5:
+                        indication = "moderately lower volatility than its peers."
+                    else:
+                        indication = "lower volatility than its peers."
                 else:
-                    indication = "higher volatility than its peers."
-            elif beta_index < 1:
-                if beta_index >= -0.5:
-                    indication = "moderately lower volatility than its peers."
-                else:
-                    indication = "lower volatility than its peers."
-            else:
-                indication = "a similar volatility to its peers."
-            st.info(f"{ticker}'s external βeta ratio of {beta_index:.2f} indicates {indication}")
-        except Exception as e:
-            st.error(f"Beta index could not be calculated: {e}")
+                    indication = "a similar volatility to its peers."
+                st.info(f"{ticker}'s external βeta ratio of {beta_index:.2f} indicates {indication}")
+            except Exception as e:
+                st.error(f"Beta index could not be calculated: {e}")
     except Exception as e:
         st.error(f"Error fetching peers for {ticker}: {e}")
     
