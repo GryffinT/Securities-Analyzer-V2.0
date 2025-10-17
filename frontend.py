967cd51aace5c685240ce82eee6d55be6d8c559c
Gryffin T <Gryffintubon@gmail.com>
2025-10-17 02:47:20 -0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -11,24 +11,16 @@ ticker = st.text_input("Enter a stock ticker symbol (e.g, AAPL, MSFT):")
 data = fetch_security_data(ticker) if ticker else None
 
 competitive_advantage = calculate_competitive_advantage(data['symbol']) if data else None
-
-def stream_data():
-    for word in (ticker.upper()).split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-    yield pd.DataFrame(
-        np.random.randn(5, 10),
-        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+if ticker:
+    st.html(
+        f"""
+        <div>
+            <h1>{ticker.upper()}</h1>
+        </div>
+        """
     )
 
-    for word in (ticker.upper()).split(" "):
-        yield word + " "
-        time.sleep(0.02)
-
-if ticker:
-    st.write_stream(stream_data)
-with st.expander("Raw yFinance data."):
-    st.write(data)
-with st.expander("Raw Finnhub data."):
-    st.write(competitive_advantage)
+    with st.expander("Raw yFinance data."):
+        st.write(data)
+    with st.expander("Raw Finnhub data."):
+        st.write(competitive_advantage)
