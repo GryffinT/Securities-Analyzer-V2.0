ab90a8c715966ad8a30ea57721e2900802e00c9a
Gryffin T <Gryffintubon@gmail.com>
2025-10-17 02:35:51 -0500

    Update competitive_advantage.py

--- competitive_advantage.py
+++ competitive_advantage.py
@@ -29,7 +29,6 @@ def calculate_competitive_advantage(ticker):
             ])
             peers = yf.screen[filter]
             st.error(f"Finhub peer data unavailable, using yfinance fallback: {e}")
-            return ""
         average_beta = 0
         for peer in peers:
             peer_info = client.company_basic_financials(peer, 'beta')
