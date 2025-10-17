e680e307847a9aad2b3aa5d546b022214c5ba590
Gryffin T <Gryffintubon@gmail.com>
2025-10-17 01:39:21 -0500

    Setup

---
+++ competitive_advantage.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from finnhub import Client
+from datetime import datetime, timedelta
+
+api_key = st.secrets["FINNHUB_API_KEY"]
+
+client = Client(api_key)
+
+def calculate_competitive_advantage(ticker):
+
+    to_date = datetime.now().strftime('%Y-%m-%d')
+    from_date = (datetime.now() - timedelta(days=60)).strftime('%Y-%m-%d')
+
+    # Ticker info
+    ticker_info = client.basic_financials(ticker, 'all')
+    ticker_insider = client.insider_sentiment(ticker, from_date, to_date)
+
+
+    try:
+        peers = client.company_peers(ticker)
+    except Exception as e:
+        st.error(f"Error fetching peers for {ticker}: {e}")
+    
+
+    return {"info": ticker_info, "insider_sentiment": ticker_insider, "peers": peers}
--- frontend.py
+++ frontend.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from fetch_security_data import fetch_security_data
+from competitive_advantage import calculate_competitive_advantage
 
 st.title("Securities Analyzer")
 ticker = st.text_input("Enter a stock ticker symbol (e.g, AAPL, MSFT):")
 data = fetch_security_data(ticker) if ticker else None
+competitive_advantage = calculate_competitive_advantage(data['symbol']) if data else None
 with st.expander("Data"):
     st.write(data)
+with st.expander("Competitive Advantage"):
+    st.write(competitive_advantage)
